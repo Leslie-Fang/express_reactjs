19ee5a8fd17f2521f02730e397d5bca6e157964d
leslie-fang <fangjiahao@sjtu.edu.cn>
2017-07-22 21:30:45 +0800

    change inout type to password

--- public/javascript/react/container/container1.js
+++ public/javascript/react/container/container1.js
@@ -69,7 +69,7 @@ class Container1 extends React.Component {
                                onFocus={this.handleusernameFocus}
                                onChange={this.handleusernameChange}/>
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="text" className="form-control" id="exampleInputPassword"
+                        <input type="password" className="form-control" id="exampleInputPassword"
                                ref="verify"
                                value={this.state.passwordValue}
                                placeHolder={this.state.defaultPasswordValue}
